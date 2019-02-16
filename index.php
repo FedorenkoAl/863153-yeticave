b91adc736fa2c98437296b9ca87f946d36e3bdc0
FedorenkoAl <alexey712003@list.ru>
2019-02-16 20:29:57 +0200

    ВЫПОЛНЕНИЕ ЗАДАНИЯ 10

--- index.php
+++ index.php
@@ -7,40 +7,40 @@ $category = [
 
 $lots = [
     [
-    'picture' => 'img/lot-1.jpg',
-    'category' => $category[0],
-    'title' => '2014 Rossignol District Snowboard',
-    'prais' => '10999'
+        'picture' => 'img/lot-1.jpg',
+        'category' => $category[0],
+        'title' => '2014 Rossignol District Snowboard',
+        'price' => '10999'
     ],
     [
-    'picture' => 'img/lot-2.jpg',
-    'category' => $category[0],
-    'title' => '2014 Rossignol District Snowboard',
-    'prais' => '15999'
+        'picture' => 'img/lot-2.jpg',
+        'category' => $category[0],
+        'title' => '2014 Rossignol District Snowboard',
+        'price' => '15999'
     ],
     [
-    'picture' => 'img/lot-3.jpg',
-    'category' => $category[1],
-    'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-    'prais' => '8000'
+        'picture' => 'img/lot-3.jpg',
+        'category' => $category[1],
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'price' => '8000'
     ],
     [
-    'picture' => 'img/lot-4.jpg',
-    'category' => $category[2],
-    'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-    'prais' => '10999'
+        'picture' => 'img/lot-4.jpg',
+        'category' => $category[2],
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'price' => '10999'
     ],
     [
-    'picture' => 'img/lot-5.jpg',
-    'category' => $category[3],
-    'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-    'prais' => '7500'
+        'picture' => 'img/lot-5.jpg',
+        'category' => $category[3],
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'price' => '7500'
     ],
     [
-    'picture' => 'img/lot-6.jpg',
-    'category' => $category[4],
-    'title' => 'Маска Oakley Canopy',
-    'prais' => '5400'
+        'picture' => 'img/lot-6.jpg',
+        'category' => $category[4],
+        'title' => 'Маска Oakley Canopy',
+        'price' => '5400'
     ]
 ];
 
--- templates/index.php
+++ templates/index.php
@@ -27,7 +27,7 @@
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?=htmlspecialchars(money($val["prais"]));?></span>
+                        <span class="lot__cost"><?=htmlspecialchars(money($val["price"]));?></span>
                     </div>
                     <div class="lot__timer timer">
                         <?=time_end(time(),strtotime('tomorrow'));?>
