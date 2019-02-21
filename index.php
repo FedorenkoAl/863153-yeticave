85b0dea35c75f21e89a81ac6ea906ec1c301b283
FedorenkoAl <alexey712003@list.ru>
2019-02-21 14:48:22 +0200

    Выполнение задания 11

--- index.php
+++ index.php
@@ -1,56 +1,41 @@
 <?php
 $is_auth = rand(0, 1);
 $user_name = 'Алексей'; // укажите здесь ваше имя
- $category = [
-    'Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'
- ];
-
- $lots = [
-     [
-        'picture' => 'img/lot-1.jpg',
-        'category' => $category[0],
-        'title' => '2014 Rossignol District Snowboard',
-        'price' => '10999'
-    ],
-     [
-        'picture' => 'img/lot-2.jpg',
-         'category' => $category[0],
-         'title' => '2014 Rossignol District Snowboard',
-         'price' => '15999'     ],
-     [
-        'picture' => 'img/lot-3.jpg',
-        'category' => $category[1],
-        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'price' => '8000'
-    ],
-    [
-        'picture' => 'img/lot-4.jpg',
-        'category' => $category[2],
-        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'price' => '10999'
-    ],
-     [
-         'picture' => 'img/lot-5.jpg',
-         'category' => $category[3],
-         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-         'price' => '7500'
-     ],
-     [
-        'picture' => 'img/lot-6.jpg',
-        'category' => $category[4],
-        'title' => 'Маска Oakley Canopy',
-        'price' => '5400'
-    ]
-];
-
-
 
 require_once('functions.php');
 
+$link = mysqli_connect('localhost', 'root', '', 'YetiCave');
+mysqli_set_charset($link, "utf8");
+    if (!$link) {
+       print('Ошибка подключения:' . mysqli_connect_error());
+    }
+    else {
+        $sql = 'SELECT name FROM category';
+        $result = mysqli_query($link, $sql);
+
+            if ($result) {
+                 $category = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            }
+            else {
+                $error = mysqli_error($link);
+                print('Ошибка MySQL' . $error);
+            }
+
+        $sql_lots = 'SELECT l.name, image, c.name cat, price FROM lots l
+        JOIN category c ON l.lots_category = c.id;';
+        $result_lots = mysqli_query($link, $sql_lots);
+            if ($result_lots) {
+                $lots = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
+            }
+            else {
+                $error = mysqli_error($link);
+                print('Ошибка MySQL' . $error);
+            }
+        }
 
 $page_content = include_template('index.php', [
-'lots' =>$lots,
- 'category' => $category
+    'lots' => $lots,
+    'category' => $category
 ]);
 
 $layout_content = include_template('layout.php', [
